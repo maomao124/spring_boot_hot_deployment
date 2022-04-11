27a4b54472a8a9a6a83f8dc035df75081680b37a
毛 <1296193245@qq.com>
2022-04-11 13:10:56 +0800

    spring_boot_hot_deployment

---
+++ src/main/java/mao/spring_boot_hot_deployment/SpringBootHotDeploymentApplication.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_hot_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHotDeploymentApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootHotDeploymentApplication.class, args);
+    }
+
+}
